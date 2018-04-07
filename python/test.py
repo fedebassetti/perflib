23e35dbdaac174c75ac8917caaa2e850d7e65450
John Emmons <emmons.john@gmail.com>
2018-04-06 18:07:44 -0700

    added build files

--- python/test.py
+++ python/test.py
@@ -4,7 +4,7 @@ print(perflib.__name__)
 print(perflib.__doc__)
 print(perflib.__version__)
 
-p = perflib.PerfCounter(counter_name="LIBPERF_COUNT_HW_INSTRUCTIONS")
+p = perflib.PerfCounter(counter_name='LIBPERF_COUNT_HW_INSTRUCTIONS')
 p.start()
 p.stop()
 
