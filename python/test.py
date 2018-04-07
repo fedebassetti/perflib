f7ec3cbe6d74a8c77917da9843d140c3e3352cdb
John Emmons <emmons.john@gmail.com>
2018-04-06 19:44:12 -0700

    improved test

--- python/test.py
+++ python/test.py
@@ -1,4 +1,5 @@
 import perflib
+import numpy as np
 
 print(perflib.__name__)
 print(perflib.__doc__)
@@ -6,11 +7,24 @@ print(perflib.__version__)
 
 print(perflib.get_available_counters())
 
+vals = []
 p = perflib.PerfCounter(counter_name='LIBPERF_COUNT_HW_INSTRUCTIONS')
-p.start()
-p.stop()
 
-print(p.getval())
+N = 10000
+for _ in range(N):
+    p.start()
+    p.stop()
+    vals.append(p.getval())
+    p.reset()
+
+vals = np.asarray(vals)
+print('min', np.min(vals))
+print('max', np.max(vals))
+
+print('median', np.median(vals))
+
+print('mean', np.mean(vals))
+print('stdev', np.std(vals))
 
 p.reset()
 
